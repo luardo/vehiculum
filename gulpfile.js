ecb2ed90f8f5efea957911b098e9cf35771d7bfc
Luis Eduardo Rivera <luardohm@gmail.com>
2019-04-13 23:28:50 -0500

    change gulp version and add responsive style adjustments

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
-var browserSync = require("browser-sync");
+var browserSync = require("browser-sync").create();
 var sassGlob = require("gulp-sass-glob");
+
+gulp.task("watch", function() {
+  gulp.watch("app/scss/**/*.scss", ["sass"]);
+});
 
 gulp.task("sass", function() {
   return gulp
@@ -9,9 +13,7 @@ gulp.task("sass", function() {
     .pipe(sassGlob())
     .pipe(sass())
     .pipe(gulp.dest("app/css"))
-    .pipe(
-      browserSync.reload({
-        stream: true
-      })
-    );
+    .pipe(browserSync.stream());
 });
+
+gulp.task("default", ["sass", "watch"]);
